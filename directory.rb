95dbedd17463f089f49ed11663c05e5580bcd3e3
Reka <rekapap28@gmail.com>
2018-09-21 18:16:17 +0100

    adds comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# print student list in new lines
 puts 'The students of Villains Academy'
 puts '-------------'
 puts 'Dr. Hannibal Lecter'
@@ -11,6 +12,8 @@ puts 'Freddy Krueger'
 puts 'The Joker'
 puts 'Joffrey Baratheon'
 puts 'Norman Bates'
+# print total number of students
 print 'Overall, we have'
+# print is inline, doesn't add newline character in the end of the  string
 print 11
 print ' great students'
